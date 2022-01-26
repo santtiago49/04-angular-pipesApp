f706a0856834caa26730e9a3e49abe42594ed79e
Santiago <santtiago49@gmail.com>
2022-01-26 11:07:10 -0300

    Time zone y otros idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { AppRouteModule } from './app-route.module';
 import { VentasModule } from './ventas/ventas.module';
 
 import localeEs from '@angular/common/locales/es-AR'
+import localeFr from '@angular/common/locales/fr-CA'
 import { registerLocaleData } from '@angular/common';
 registerLocaleData( localeEs );
+registerLocaleData( localeFr );
 
 @NgModule({
   declarations: [
