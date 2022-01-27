d6817d2692b556436d412028c6338d8601ed032e
Santiago <santtiago49@gmail.com>
2022-01-27 20:10:08 -0300

    PrimeNg - FieldSet

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {MenubarModule} from 'primeng/menubar';
+import {FieldsetModule} from 'primeng/fieldset';
+
 
 
 
@@ -11,6 +13,7 @@ import {MenubarModule} from 'primeng/menubar';
   exports: [
     ButtonModule,
     CardModule,
+    FieldsetModule,
     MenubarModule
   ]
 })
